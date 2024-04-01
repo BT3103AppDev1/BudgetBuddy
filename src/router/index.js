627d0ff902466827bbfd3052b26a10ae71eb938d
lershuan <lershuan@gmail.com>
2024-04-01 18:09:18 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "@/components/Login.vue";
-import Home from "@/views/Home.vue";
-import Profile from "@/views/Profile.vue";
-import NotFound from "@/views/NotFound.vue";
 
 const routes = [
   {
     path: "/",
     name: "Login",
     component: Login,
-  },
-  {
-    path: "/home",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/profile",
-    name: "Profile",
-    component: Profile,
-  },
-  {
-    path: "/:catchAll(.*)",
-    name: "NotFound",
-    component: NotFound,
   },
 ];
 
