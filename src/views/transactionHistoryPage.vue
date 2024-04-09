a7c7a0716fe1ecbb5b361021186ae5eb90a4bd3a
lershuan <lershuan@gmail.com>
2024-04-09 17:19:12 +0800

    add logout to every page

--- src/views/transactionHistoryPage.vue
+++ src/views/transactionHistoryPage.vue
@@ -1,16 +1,35 @@
 <template>
   <sidebar />
   <transactionHistory></transactionHistory>
+  <Logout :user="user" />
 </template>
 
 <script>
 import sidebar from "../components/sidebar.vue";
 import transactionHistory from "../components/transactionHistory.vue";
+import Logout from "@/components/Logout.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 export default {
   name: "addBudgetPage",
   components: {
     transactionHistory,
     sidebar,
+    Logout,
+  },
+  data() {
+    return {
+      user: null,
+      userEmail: "nothing",
+    };
+  },
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+        this.userEmail = user.email;
+      }
+    });
   },
 };
 </script>
