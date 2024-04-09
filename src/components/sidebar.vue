0a9f8a1d6d1a78dc2ae139f4e7ba58d63ff8fb72
yinshuennn <122251607+yinshuennn@users.noreply.github.com>
2024-04-10 01:16:02 +0800

    updated styling of sidebar

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="sidebar">
+    <h2>BudgetBuddy</h2>
     <nav>
-      <ul>
-        <li>
+      <ul class="menu">
+        <li class="item" :class="{ 'active': $route.path === '/dashboardPage' }">
+          <ion-icon name="list-circle"></ion-icon>
           <router-link to="/dashboardPage">Dashboard</router-link>
         </li>
-        <li>
+        <li class="item" :class="{ 'active': $route.path === '/addTransaction' }">
+          <ion-icon name="card"></ion-icon>
           <router-link to="/addTransaction">Add Transaction</router-link>
         </li>
-        <li>
+        <li class="item" :class="{ 'active': $route.path === '/addBudget' }">
+          <ion-icon name="chatbubble-ellipses"></ion-icon>
           <router-link to="/addBudget">Add Budget</router-link>
         </li>
-        <li>
-          <router-link to="/addScheduledTransaction"
-            >Add Scheduled Transaction</router-link
-          >
+        <li class="item" :class="{ 'active': $route.path === '/addScheduledTransaction' }"> 
+          <ion-icon name="alarm"></ion-icon>
+          <router-link to="/addScheduledTransaction">Add Scheduled Transactions</router-link>
         </li>
-        <li>
-          <router-link to="/transactionHistory"
-            >Transaction History</router-link
-          >
+        <li class="item" :class="{ 'active': $route.path === '/transactionHistory' }">
+          <ion-icon name="calendar"></ion-icon>
+          <router-link to="/transactionHistory">Transaction History</router-link>
+        </li>
+        <li class="item" :class="{ 'active': $route.path === '/userProfile' }">
+          <ion-icon name="person-circle"></ion-icon>
+          <router-link to="/userProfile">User Profile</router-link>
         </li>
         <!-- Other sidebar items -->
       </ul>
@@ -37,27 +43,46 @@ export default {
 <style scoped>
 /* Basic sidebar styles */
 .sidebar {
-  width: 250px; /* or however wide you want it */
+  width: 270px; /* or however wide you want it */
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   background-color: #f5f5f5; /* Light grey background */
+  overflow: hidden;
+  transition: all 0.5s linear;
   /* Additional styling */
 }
-
-.sidebar ul {
-  list-style-type: none;
+.menu {
+  position: relative;
+  list-style: none;
   padding: 0;
 }
-
-.sidebar li {
-  padding: 10px;
-  border-bottom: 1px solid #ddd; /* Light grey border */
+.menu a {
+  color: #474745;
+  text-decoration: none;
 }
-
-/* Style the active route */
-.router-link-active {
-  background-color: #e0e0e0; /* Slightly darker grey */
+.menu li {
+  padding: 20px;
+  margin: 8px 0;
+  border-radius: 8px;
+  transition: all 0.5s ease-in-out;
+  list-style: none;
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+}
+.menu li:hover {
+  background: #c5c4c458;
+}
+.menu .item.active {
+  background-color: #c5c4c458; /* Slightly darker grey */
+}
+ion-icon{
+  color:#959BA2;
+  height: 20px;
+  width: 20px;
+  margin-right: 5px;
 }
 </style>
