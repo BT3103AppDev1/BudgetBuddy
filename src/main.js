8516686a5c629ba420b61bc5cfb5ecc118399ecf
Keshia Hanna Lim <e0960304@u.nus.edu>
2024-04-18 23:21:00 +0800

    user not logged out upon refresh

--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
-//import firebase from 'firebase';
+import firebaseApp from './firebase';
 //import firebaseui from 'firebaseui';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 // Create the Vue app instance
 const app = createApp(App);
 
+// Initialize Firebase
+const auth = getAuth(firebaseApp);
+let hasInitialized = false;
+
+onAuthStateChanged(auth, (user) => {
+    if (!hasInitialized) {
+        hasInitialized = true;
+        if (user) {
+            console.log("User is signed in");
+            app.config.globalProperties.$currentUser = user;
+        } else {
+            console.log("User is signed out");
+            app.config.globalProperties.$currentUser = null;
+        }
+        app.use(router).mount('#app');
+    }
+});
+
+
 // Use Vue Router
-app.use(router);
+//app.use(router);
 
 // Mount the app
-app.mount("#app");
+//app.mount("#app");
