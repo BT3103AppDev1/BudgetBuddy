4d9ba68daa66fd18e5ec1dd307b75eed95a2b8a1
Keshia Hanna Lim <e0960304@u.nus.edu>
2024-04-25 02:09:17 +0800

    analytics page top transactions + filters + debugging

--- src/components/top3spendings.vue
+++ src/components/top3spendings.vue
@@ -1,137 +1,145 @@
 <template>
-  <div>
-    <h2>Top 3 Spendings</h2>
-    <div class="topSpendings">
-      <ul>
-        <li
-          v-for="(spending, index) in topSpendings"
-          :key="index"
-          class="transaction-item"
-        >
-          <div class="transaction-details">
-            <h3 class="transaction-name">{{ spending.name }}</h3>
-            <p class="transaction-date">{{ spending.date }}</p>
-            <p class="transaction-amt">{{ spending.amount }}</p>
-          </div>
-        </li>
-      </ul>
+  <div class="analytics-container">
+    <div class="spendings-container">
+      <h2>Top Spendings</h2>
+      <div class="filter-buttons">
+        <button @click="setFilter('week')" :class="{ active: currentFilter === 'week' }">Week</button>
+        <button @click="setFilter('month')" :class="{ active: currentFilter === 'month' }">Month</button>
+        <button @click="setFilter('year')" :class="{ active: currentFilter === 'year' }">Year</button>
+      </div>
+      <div v-if="filteredTransactions.length > 0" class="top-spendings-list">
+        <ul>
+          <li v-for="(transaction, index) in filteredTransactions" :key="index" class="spending-item">
+            <div class="spending-details">
+              <h3 class="spending-name">{{ transaction.name }}</h3>
+              <p class="spending-date">{{ transaction.date }}</p>
+              <p class="spending-amount">{{ -transaction.amount }}</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <p v-else>No transactions available.</p>
     </div>
   </div>
 </template>
 
 <script>
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import firebaseApp from "../firebase.js";
-import { getAuth } from "firebase/auth";
+import { startOfWeek, startOfMonth, startOfYear, endOfWeek, endOfMonth, endOfYear, parseISO } from 'date-fns';
+import { getFirestore, collection, query, where, orderBy, limit, getDocs } from 'firebase/firestore';
+import firebaseApp from '../firebase.js';
+import { getAuth } from 'firebase/auth';
 
 export default {
   data() {
     return {
-      topSpendings: [],
+      transactions: [],
+      filteredTransactions: [],
+      currentFilter: 'week',
     };
   },
   async mounted() {
-    await this.fetchAndComputeTopSpendings();
+    await this.fetchTransactions();
+    this.setFilter(this.currentFilter);
   },
   methods: {
-    async fetchAndComputeTopSpendings() {
+    async fetchTransactions() {
+      const db = getFirestore(firebaseApp);
+      const auth = getAuth(firebaseApp);
+      const user = auth.currentUser;
+
+      if (!user) {
+        console.error("No user logged in!");
+        return;
+      }
+
+      const userId = user.uid;
+      const transactionsCol = collection(db, "users", userId, "transactions");
+      const q = query(transactionsCol, orderBy("date", "desc"), limit(50)); // Adjust limit as needed
+
       try {
-        const db = getFirestore(firebaseApp);
-        const auth = getAuth(firebaseApp);
-        const user = auth.currentUser;
-        if (user) {
-          console.log("User ID:", user.uid); // Make sure you can retrieve the user ID
-        }
-        if (!user) {
-          console.error("No user logged in!");
-          return;
-        }
-        const userId = user.uid;
-        const transactionsCol = collection(db, "users", userId, "transactions");
-        const querySnapshot = await getDocs(transactionsCol);
-
-        const aggregatedSpendings = {};
-        querySnapshot.forEach((doc) => {
-          const transaction = doc.data();
-          const dateParts = transaction.date.split("-"); // Split date string by '-'
-          const year = dateParts[0];
-          const month = dateParts[1];
-          const day = dateParts[2];
-          const formattedDate = `${year}-${month}-${day}`; // Reformat date if needed
-          const name = transaction.name;
-
-          if (!aggregatedSpendings[formattedDate]) {
-            aggregatedSpendings[formattedDate] = {
-              totalAmount: 0,
-              transactions: [],
-            };
-          }
-          aggregatedSpendings[formattedDate].totalAmount += transaction.amount;
-          aggregatedSpendings[formattedDate].transactions.push({
-            name: transaction.name,
-            amount: transaction.amount,
-          });
-        });
-
-        const spendingsArray = Object.keys(aggregatedSpendings).map((date) => {
-          const formattedDate = formatDate(date);
-          return {
-            date: formattedDate,
-            totalAmount: aggregatedSpendings[date].totalAmount,
-            transactions: aggregatedSpendings[date].transactions,
-          };
-        });
-
-        // Function to format date string to "YYYY-MM-DD" format
-        function formatDate(dateString) {
-          const dateParts = dateString.split("-");
-          const year = dateParts[0];
-          const month = dateParts[1].padStart(2, "0"); // Ensure month is zero-padded
-          const day = dateParts[2].padStart(2, "0"); // Ensure day is zero-padded
-          return `${year}-${month}-${day}`;
-        }
-
-        this.topSpendings = spendingsArray
-          .sort((a, b) => b.amount - a.amount)
-          .slice(0, 3);
+        const querySnapshot = await getDocs(q);
+        this.transactions = querySnapshot.docs.map(doc => doc.data());
+        console.log("Fetched transactions:", this.transactions);
       } catch (error) {
-        console.error("Error fetching and computing top spendings:", error);
+        console.error("Error fetching transactions:", error);
       }
     },
+    setFilter(filterType) {
+      this.currentFilter = filterType;
+      this.filterTransactions();
+    },
+    filterTransactions() {
+      const now = new Date();
+      const startOfPeriod = {
+        'week': startOfWeek(now),
+        'month': startOfMonth(now),
+        'year': startOfYear(now)
+      };
+      const endOfPeriod = {
+        'week': endOfWeek(now),
+        'month': endOfMonth(now),
+        'year': endOfYear(now)
+      };
+      const start = startOfPeriod[this.currentFilter];
+      const end = endOfPeriod[this.currentFilter];
+
+      this.filteredTransactions = this.transactions.filter(t => {
+        const date = parseISO(t.date);
+        return date >= start && date <= end;
+      }).sort((a, b) => a.amount - b.amount).slice(0, 3);
+    }
   },
 };
 </script>
 
+
 <style scoped>
-.spendingList {
-  margin: 0;
+
+.filter-buttons {
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+.filter-buttons button {
+  padding: 10px 20px;
+  border: none;
+  background-color: #332f2f;
+  cursor: pointer;
+  border-radius: 5px; /* Rounded corners for buttons */
+}
+
+.filter-buttons button.active {
+  background-color: #d0d0d0; /* Slightly darker background for active button */
+}
+.spendings-container {
+  max-width: 600px;
+  margin: auto;
+}
+
+.top-spendings-list {
+  list-style: none;
   padding: 0;
 }
 
-.transaction-item {
-  background-color: #f9f9f9; /* Light grey background */
+.top-spendings-list ul {
+  list-style: none;
+  padding: 0;
+}
+
+
+.spending-item {
+  background-color: #f9f9f9;
   border-radius: 8px;
   padding: 15px;
-  margin-bottom: 10px; /* Space between items */
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Subtle shadow for depth */
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: auto;
+  margin-bottom: 10px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
 
-.transaction-details {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+.spending-details {
   text-align: center;
-  flex: 1;
-  height: 100px;
 }
 
-.transaction-amount {
-  align-items: flex-end;
+.spending-amount {
   font-size: 1rem;
   font-weight: bold;
 }
